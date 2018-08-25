ff9a4a195bd088dc9f1cdf67db8a16b0a8b5c49a
yael navon <yael@yaels-MacBook-Air.local>
2018-08-25 12:29:30 +1000

    first commit1

--- apps/vehicle-manufacture/server/static-routes.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (app) {
-    require('./static-routes/vehicle-manufacture-car-builder')(app);
-    require('./static-routes/vehicle-manufacture-manufacturing')(app);
-    require('./static-routes/vehicle-manufacture-regulator')(app);
-}
